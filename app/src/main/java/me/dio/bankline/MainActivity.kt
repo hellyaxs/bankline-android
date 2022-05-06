30a4b4a8c1b9500744e6d529670f6acbc8fb0abd
hellyaxs <eliasvitor.dev@gmail.com>
2022-05-06 12:20:09 -0300

    app com build funcional

--- app/src/main/java/me/dio/bankline/MainActivity.kt
+++ app/src/main/java/me/dio/bankline/MainActivity.kt
@@ -1,11 +1,14 @@
 package me.dio.bankline
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import me.dio.bankline.databinding.ActivityMainBinding
 import me.dio.bankline.domain.Correntista
+import me.dio.bankline.ui.statement.BankStatementActivity
 
 class MainActivity : AppCompatActivity() {
+
     private val binding by lazy{
         ActivityMainBinding.inflate(layoutInflater)
     }
@@ -14,9 +17,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        //acessando o botão de continua e fazendo uma ação quando ele for clikcado
         binding.button.setOnClickListener {
             val accountHouderId = binding.editTextNumber.toString().toInt()
             val accountHolder = Correntista(accountHouderId)
+
+            //o intent esta enviando o nosso correntista para a tela BankStatement
+            val intent = Intent(this,BankStatementActivity::class.java).apply {
+                putExtra(BankStatementActivity.EXTRA_ACCOUNT_HOLDER,accountHolder)
+            }
+            startActivity(intent)
         }
 
     }
--- app/src/main/java/me/dio/bankline/ui/statement/BankStatementActivity.kt
+++ app/src/main/java/me/dio/bankline/ui/statement/BankStatementActivity.kt
@@ -3,10 +3,28 @@ package me.dio.bankline.ui.statement
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import me.dio.bankline.R
+import me.dio.bankline.databinding.ActivityBankStatementBinding
+import me.dio.bankline.domain.Correntista
 
 class BankStatementActivity : AppCompatActivity() {
+
+    companion object {
+        const val  EXTRA_ACCOUNT_HOLDER = "me.dio.bankline.ui.statement.EXTRA_ACCOUNT_HOLDER"
+    }
+    //com essa variavel podemos acessar qualquer elemento do meu layout
+    private val binding by lazy {
+        ActivityBankStatementBinding.inflate(layoutInflater)
+    }
+    private val correntita by lazy {
+       intent.getParcelableExtra<Correntista>(EXTRA_ACCOUNT_HOLDER) ?: throw IlegalArgumentException()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_bank_statement)
+        setContentView(binding.root)
     }
+}
+
+class IlegalArgumentException : Throwable() {
+
 }
